8a0928bdd76b5edcceaf740a360bdc39ef837deb
dnsfer <dnunesferreira.fr@gmail.com>
2024-11-03 22:48:00 -0300

    git add .

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit' , Event => {
+    Event.preventDefault();
+    const FirsName = document.querySelector('#firsName');
+    console.log(firstName.value)
+})
